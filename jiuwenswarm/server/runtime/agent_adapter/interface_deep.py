da784d4e5001f6f9e163814cf332f25340899f49
Michael <mishat@gmail.com>
2026-08-31 14:38:44 +0300

    Fix

--- jiuwenswarm/server/runtime/agent_adapter/interface_deep.py
+++ jiuwenswarm/server/runtime/agent_adapter/interface_deep.py
@@ -1294,6 +1294,7 @@ class JiuWenSwarmDeepAdapter:
         self._permission_rail: Any = None
         self._avatar_rail: Any = None
         self._memory_forbidden_rail: Any = None
+        self._tool_call_deduplication_rail: ToolCallDeduplicationRail | None = None
         self._tool_cards = None
         self._evolution_watcher_tasks: set[asyncio.Task] = set()
         self._sys_operation = None
